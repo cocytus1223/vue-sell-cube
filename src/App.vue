b2cd445ee93f1608c4ad255945b3208de5f1597f
Nocky <cocytus1223@gmail.com>
2019-01-06 21:27:36 +0800

    tab栏完成

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,18 @@
   <div id="app">
     <!-- 头部内容 -->
     <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs" :initialIndex=1></tab>
+    </div>
   </div>
 </template>
 
 <script>
 import VHeader from 'components/v-header/v-header'
+import Goods from 'components/goods/goods'
+import Seller from 'components/seller/seller'
+import Ratings from 'components/ratings/ratings'
+import Tab from 'components/tab/tab'
 import { getSeller } from 'api'
 export default {
   name: 'app',
@@ -21,9 +28,41 @@ export default {
     })
   },
   components: {
-    VHeader
+    VHeader,
+    Tab
+  },
+  computed: {
+    tabs () {
+      return [
+        { label: '商品',
+          component: Goods,
+          data: {
+            seller: this.seller
+          }
+        },
+        { label: '评价',
+          component: Ratings,
+          data: {
+            seller: this.seller
+          }
+        },
+        { label: '商家',
+          component: Seller,
+          data: {
+            seller: this.seller
+          }
+        }
+      ]
+    }
   }
 }
 </script>
-<style lang="stylus">
+<style lang="stylus" scoped>
+#app
+  .tab-wrapper
+    position fixed
+    top 136px
+    left 0
+    right 0
+    bottom 0
 </style>
---
+++ src/components/goods/goods.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    i am goods
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'goods'
+}
+</script>
+
+<style scoped>
+</style>
